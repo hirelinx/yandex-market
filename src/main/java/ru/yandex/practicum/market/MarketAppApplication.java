78b34b3d5daae9671d157ba3ab4ba456935805e4
Alex Grishin <53r10u5p3r50n@gmail.com>
2026-09-13 11:43:25 +0300

    feat(app): add MarketAppApplication

---
+++ src/main/java/ru/yandex/practicum/market/MarketAppApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketAppApplication.class, args);
+	}
+
+}
