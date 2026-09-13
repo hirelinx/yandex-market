070d4d233eea9c4691a5e48531cc66af6fedd3cc
Alex Grishin <53r10u5p3r50n@gmail.com>
2026-09-13 12:22:47 +0300

    feat(web): add request POJO

---
+++ src/main/java/ru/yandex/practicum/market/web/request/ItemCreateRequest.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.market.web.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemCreateRequest {
+    @NotBlank(message = "Название обязательно")
+    private String title;
+
+    @NotBlank(message = "Описание обязательно")
+    private String description;
+
+    @NotNull(message = "Цена обязательна")
+    @DecimalMin(value = "0.01", message = "Цена должна быть больше 0")
+    private BigDecimal price;
+}
